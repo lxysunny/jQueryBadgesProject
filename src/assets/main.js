6e76254150c3561805335159c800e474055ad13b
lxysunny <lxy.sunny@gmail.com>
2017-07-01 19:03:48 +0100

    add course

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,34 @@
 $(function() {
+  $.ajax({
+  url:'https://www.codeschool.com/users/2113335.json',
+  dataType: 'jsonp',
+  success: function(response) {
+    //console.log('response', response.courses.in_progress);
+    addCourses(response.courses.in_progress);
+  }
+});
 
-  // your code will go here
+  function addCourses(courses) {
+    var $badges = $('#badges');
+    courses.forEach(function(course) {
+        var $course = $('<div />', {
+          'class':'course'
+        }).appendTo($badges);
 
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($course);
+
+        $('<img />', {
+          src: course.badge
+        }).appendTo($course);
+
+        $('<a />', {
+          class: 'btn btn-primary',
+          target: '_blank',
+          herf: course.url,
+          text: 'See Course'
+        }).appendTo($course);
+    })
+  }
 });
